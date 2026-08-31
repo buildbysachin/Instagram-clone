a16c9b535a91b14213fdce47da9463fca6cdf1c8
buildbysachin <buildbysachin@gmail.com>
2026-08-31 16:25:31 +0530

    change favicon

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Slidebar from "@/component/navigation/slidebar";
-import MobileBottomBar from "@/component/navigation/mobileBottomBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
